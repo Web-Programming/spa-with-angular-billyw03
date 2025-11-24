61af1e65562b1a7253d60fb385779eccf555186a
billyw03 <yohanesbillywicaksono_2327240155@mhs.mdp.ac.id>
2025-11-24 20:40:01 +0700

    Implement housing service, search, and pagination

--- griya-mdp-backend/app_server/models/db.js
+++ griya-mdp-backend/app_server/models/db.js
@@ -1,19 +1,18 @@
 let mongoose = require("mongoose");
-let dbURI = "mongodb+srv://paw2:si@paw2.iendmj6.mongodb.net/PAWII-SI?retryWrites=true&w=majority&appName=paw2";
+// let dbURI = "mongodb+srv://paw2:si@paw2.iendmj6.mongodb.net/PAWII-SI?retryWrites=true&w=majority&appName=paw2";
+// let dbURI = "mongodb://localhost:27017/namadb"
+const dbURI = "mongodb://localhost:27017/paw2A";
 
-mongoose.connect(dbURI,{
-    //useNewURLParser: true
-});
+mongoose.connect(dbURI, {});
 
 mongoose.connection.on("connected", () => {
-    console.log("Connected To MongoDB");
+  console.log(`Mongoose connected to ${dbURI}`);
 });
 
-mongoose.connection.on("error", (error) => {
-    console.log("Connection Error: " + error);
+mongoose.connection.on("error", (err) => {
+  console.log("Mongoose connection error:", err);
 });
 
-mongoose.connection.on("disconected", () => {
-    console.log("Disconnected From MongoDB");
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongoose disconnected");
 });
-
